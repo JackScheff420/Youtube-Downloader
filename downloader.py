30230010bbda461ed3be906fdba1ea8a5b36baf9
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2022-05-18 16:12:53 +0200

    download funktion gemacht

---
+++ downloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+url = 'https://www.youtube.com/watch?v=2MFO66voVm0'
+my_video = YouTube(url)
+
+print("####################### Video Title ############################")
+print (my_video.title)
+
+print("####################### Thumbnail ############################")
+print (my_video.thumbnail_url)
+
+my_video = my_video.streams.get_audio_only()
+my_video.download()
