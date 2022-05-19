817deab9c82c36c289a5083dedca8879d4cd327e
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2022-05-19 08:29:18 +0200

    mp3 und mp4 option eingebaut

--- downloader.py
+++ downloader.py
@@ -1,6 +1,8 @@
 ##Maxcode##
 import tkinter as tk
 from pytube import YouTube
+import os
+
 
 url = input("Your YouTube url: ")
 video = YouTube(url)
@@ -11,17 +13,32 @@ print(video.title)
 print("********************Tumbnail Image***********************")
 print(video.thumbnail_url)
 
-print("********************Download video*************************")
-#stream = video.streams.get_highest_resolution()
-stream = video.streams.get_audio_only()
 
 
-#Download video
-stream.download()
+eingabe = input("mp3 oder mp4?")
+if eingabe == 'mp3':
 
-root = tk.Tk()
-root.title("YouTube Mp3/Mp4 downloader")
-root.geometry('600x400+50+50')
+    stream = video.streams.get_audio_only()
+    stream.download()
+
+    mp4_file = os.path.join("H:\Eigene Dateien\Arbeit\Programmieren\Python\Youtube-Downloader", video.title + ".mp4")
+    mp3_file = os.path.join("H:\Eigene Dateien\Arbeit\Programmieren\Python\Youtube-Downloader", video.title + ".mp3")
+    os.rename(mp4_file, mp3_file)
+
+elif eingabe == 'mp4':
+
+    stream = video.streams.get_highest_resolution()
+    stream.download()
+
+else:
+    exit()
 
 
-root.mainloop()
+
+
+# root = tk.Tk()
+# root.title("YouTube Mp3/Mp4 downloader")
+# root.geometry('600x400+50+50')
+
+
+# root.mainloop()
