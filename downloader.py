eb2238f82589b8564ce21c64f3e6249b27e1cfc5
Maximilian Scheffler <Maximilian.Scheffler@alho.com>
2022-07-28 10:58:28 +0200

    playlist download funktion hinzugefügt

--- downloader.py
+++ downloader.py
@@ -1,37 +1,60 @@
 ##Maxcode##
 import tkinter as tk
+from turtle import down
 from pytube import YouTube
+from pytube import Playlist
 import os
 
 
-url = input("Your YouTube url: ")
-video = YouTube(url)
-
-print("*********************Video Title************************")
-print(video.title)
-
-print("********************Tumbnail Image***********************")
-print(video.thumbnail_url)
 
 
 
-eingabe = input("mp3 oder mp4?")
-if eingabe == 'mp3':
 
-    stream = video.streams.get_audio_only()
-    stream.download()
+def download_plist(p):
+    purl = Playlist(p)
 
-    mp4_file = os.path.join("H:\Eigene Dateien\Arbeit\Programmieren\Python\Youtube-Downloader", video.title + ".mp4")
-    mp3_file = os.path.join("H:\Eigene Dateien\Arbeit\Programmieren\Python\Youtube-Downloader", video.title + ".mp3")
-    os.rename(mp4_file, mp3_file)
+    print(f'Downloading: {p.title}')
 
-elif eingabe == 'mp4':
+    for video in purl.videos:
+        print(video.title)
+        st = video.streams.get_highest_resolution()
+        st.download()
 
-    stream = video.streams.get_highest_resolution()
-    stream.download()
 
-else:
-    exit()
+
+
+download_plist(input("Your YouTube url: "))
+
+# video = YouTube(url)
+
+# print("*********************Video Title************************")
+# print(video.title)
+
+# print("********************Tumbnail Image***********************")
+# print(video.thumbnail_url)
+
+
+
+# eingabe = input("mp3 oder mp4? ")
+# if eingabe == 'mp3':
+
+#     stream = video.streams.get_audio_only()
+#     stream.download()
+
+#     mp4_file = os.path.join("H:\Eigene Dateien\Arbeit\Programmieren\Python\Youtube-Downloader", video.title + ".mp4")
+#     mp3_file = os.path.join("H:\Eigene Dateien\Arbeit\Programmieren\Python\Youtube-Downloader", video.title + ".mp3")
+#     os.rename(mp4_file, mp3_file)
+
+# elif eingabe == 'mp4':
+#     stream = video.streams.get_highest_resolution()
+#     stream.download()
+
+# else:
+#     exit()
+
+
+
+
 
 
 
